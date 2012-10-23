3a13b08d53634cc9db9d1770174780d084ead8fe
Joao Sousa <alpalhao@gmail.com>
2012-10-23 22:32:34 +0100

    Starting XML Viewer

---
+++ XmlViewer/XmlViewer/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace XmlViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textEditorControl1 = new ICSharpCode.TextEditor.TextEditorControl();
+            this.SuspendLayout();
+            // 
+            // textEditorControl1
+            // 
+            this.textEditorControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textEditorControl1.IndentStyle = ICSharpCode.TextEditor.Document.IndentStyle.Auto;
+            this.textEditorControl1.IsIconBarVisible = false;
+            this.textEditorControl1.Location = new System.Drawing.Point(0, 0);
+            this.textEditorControl1.Name = "textEditorControl1";
+            this.textEditorControl1.ShowInvalidLines = false;
+            this.textEditorControl1.ShowMatchingBracket = false;
+            this.textEditorControl1.Size = new System.Drawing.Size(754, 588);
+            this.textEditorControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 588);
+            this.Controls.Add(this.textEditorControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ICSharpCode.TextEditor.TextEditorControl textEditorControl1;
+    }
+}
+
